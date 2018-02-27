3d3c1b36672347ddd6cb487fb91ec91a167f823b
parth <srivastavaparth2016@gmail.com>
2018-02-27 21:26:12 +0530

    adding test case for /getAll

--- test/src/routes/getAll.test.js
+++ test/src/routes/getAll.test.js
@@ -1,4 +1,5 @@
 const server = require('../../../src/server');
+const Models = require('../../../models');
 
 describe('checking for api endpoint /getAll', () => {
   it('checking for the status code', () => {
@@ -8,6 +9,25 @@ describe('checking for api endpoint /getAll', () => {
     };
     server.inject(options, (response) => {
       expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe('checking if the server works or not', () => {
+  afterAll((done) => {
+    Models.questions.destroy({ truncate: true }).then(() => {
+      console.log('deleted wohoo');
+      done();
+    }).catch(console.log);
+  });
+  test('checking if the status code is 200 or not', (done) => {
+    const options = {
+      url: '/getAll',
+      method: 'POST',
+    };
+    server.inject(options, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
     });
   });
 });
