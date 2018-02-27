6e86a0693a05fcfdd8a41afaec48ca15d0c42a47
parth <srivastavaparth2016@gmail.com>
2018-02-27 21:22:03 +0530

    test case for the store function

---
+++ src/helpers/store.js
@@ -0,0 +1,27 @@
+const getQWithA = require('./getQWithA');
+const Models = require('../../models');
+
+const store = () => getQWithA().then((results) => {
+  const dataToBeStored = results.map((result) => {
+    const keys = Object.keys(result);
+    let options = '';
+    for (let i = 0; i < keys.length; i += 1) {
+      if (keys[i] !== 'question' && keys[i] !== 'questionId' && keys[i] !== 'rightAnswer') {
+        options += `${result[keys[i]]}, `;
+      }
+    }
+    console.log(options, result.question);
+
+    const newQuestion = {
+      question: result.question,
+      questionid: result.questionId,
+      options,
+      rightans: result.rightAnswer,
+    };
+    return newQuestion;
+  });
+  Models.questions.destroy({ truncate: true });
+  return Models.questions.bulkCreate(dataToBeStored).then(() => { console.log('database made'); });
+});
+
+module.exports = store;
---
+++ test/src/helpers/store.test.js
@@ -0,0 +1,24 @@
+const Models = require('../../../models');
+
+describe('checking if the database is properly working or not', () => {
+  afterAll((done) => {
+    Models.questions.destroy({
+      truncate: true,
+    }).then(() => {
+      done();
+    }).catch(console.log);
+  });
+  it('checking if the inserting of data works or not', (done) => {
+    Models.questions.create({
+
+      question: 'question',
+      questionid: 1,
+      options: 'hi',
+      rightans: 'rightans',
+
+    }).then(() => 'true').then((value) => {
+      console.log(value);
+      done();
+    });
+  });
+});
