1a6d204e79b850d63e2053a294dbd9e0753b029f
parth <srivastavaparth2016@gmail.com>
2018-02-27 20:15:56 +0530

    test case 2 to check if the promise is returned or not for helper function

--- test/src/helpers/getQWithA.test.js
+++ test/src/helpers/getQWithA.test.js
@@ -2,5 +2,8 @@ const getQWithA = require('../../../src/helpers/getQWithA');
 
 describe('checking if the helper function for getting data from the two apis work or not', () => {
   it('checking if the data returned is an object or not', () => expect(typeof getQWithA()).toBe('object'));
+  it('checking if the data returned is an promise or not', () => {
+    getQWithA().then(value => expect(typeof value).toBe('object'));
+  });
 });
 
